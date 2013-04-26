7511378a96fd491186c7b80b556f8d8a6e5fd1fa
saumyadalal <sdalal@andrew.cmu.edu>
2013-04-25 22:19:48 -0400

    Added macros to cache.c to set it up a bit

--- proxylab-handout/cache.c
+++ proxylab-handout/cache.c
@@ -1,3 +1,13 @@
-#
-#
-#
+/* Defining macros for the proxy's maximum cache size
+   and the maximum size of web objects to be cached */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+#include "cache.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+
