92c32ba2f754419457455e803888ce537ea1389b
alexslucena <alex.s.lucena@gmail.com>
2013-04-29 20:43:13 -0400

    Reading in Host: header and sending it forward, along with rest of Lab's headers

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -21,13 +21,13 @@
 #endif
 
 static const char *user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-static const char *accept_ = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *accept_type = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding = "Accept-Encoding: gzip, deflate\r\n";
 
 void serve(int file_d);
-void read_headers(rio_t *rp);
+void read_headers(rio_t *rp, char* host_header);
 int parse_url(char *url, char *host, char *path, char *cgiargs);
-void make_request(int fd, char *host, char *path);
+void make_request(int fd, char *host, char *path, char *host_header);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 void terminate(int param);
 
@@ -38,7 +38,7 @@ void terminate(int param);
 int main(int argc, char **argv)
 {
     printf("-------- START PROXY INFO --------\r\n");
-    printf("%s%s%s", user_agent, accept_, accept_encoding);
+    printf("%s%s%s", user_agent, accept_type, accept_encoding);
     printf("--------- END PROXY INFO ---------\r\n");
 
     int listenfd, connfd, port, clientlen;
@@ -59,7 +59,7 @@ int main(int argc, char **argv)
     {
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA *) &clientaddr, (socklen_t *) &clientlen);
-dbg_printf("\nDEBUG CHECK\n");
+        dbg_printf("\nDEBUG CHECK\n");
         serve(connfd);
         Close(connfd);
     }
@@ -75,6 +75,7 @@ dbg_printf("\nDEBUG CHECK\n");
  {
     char method[MAXLINE], url[MAXLINE], version[MAXLINE];
     char buf[MAXLINE], host[MAXLINE], path[MAXLINE], cgiargs[MAXLINE];
+    char host_header[MAXLINE];
     rio_t rio;
 
     /* Read in a request from client */
@@ -92,14 +93,19 @@ dbg_printf("\nDEBUG CHECK\n");
         return;
     }
 
-    read_headers(&rio);
+
+    printf("\n<><><><><><><><><><><><><><><><><><>\n");
+    printf("<><><><><><><><><><><><><><><><><><>\n");
+
+
+    read_headers(&rio, host_header);
 
     // Parse URL out of request
     dbg_printf("PRE-PARSE\n");
     parse_url(url, host, path, cgiargs);
     dbg_printf("POST-PARSE\n");
 
-    make_request(file_d, host, path);
+    make_request(file_d, host, path, host_header);
 
 
  }
@@ -141,7 +147,7 @@ void terminate (int param)
 /*
 * Reads in an ignores headers of requests
 */
-void read_headers(rio_t *rp)
+void read_headers(rio_t *rp, char *host_header)
 {
    char buf[MAXLINE];
 
@@ -152,6 +158,11 @@ void read_headers(rio_t *rp)
    {
         Rio_readlineb(rp, buf, MAXLINE);
         printf("%s", buf);
+
+        int prefix = strlen("Host: ");
+
+	if (!strncmp(buf, "Host: ", prefix))
+            strcpy(host_header, buf + prefix);
    }
 
    dbg_printf("--------\nDone with headers\n");
@@ -196,7 +207,7 @@ int parse_url(char *url, char *host, char *path, char *cgiargs)
 }
 
 
-void make_request(int fd, char *host, char *path)
+void make_request(int fd, char *host, char *path, char *host_header)
 {
 
     int net_fd;
@@ -205,7 +216,18 @@ void make_request(int fd, char *host, char *path)
 
     net_fd = Open_clientfd(host, 80);
 
-    sprintf(buf, "GET %s HTTP/1.0\r\n\r\n", path);
+    sprintf(buf, "GET %s HTTP/1.0\r\n", path);
+    printf("Send request buf: \n%s\n", buf);
+
+    if (host_header == NULL)
+        sprintf(buf, "%sHost: %s\r\n", buf, host);
+    else
+        sprintf(buf, "%sHost: %s\r\n", buf, host_header);
+    sprintf(buf, "%sUser-Agent: %s\r\n", buf, user_agent);
+    sprintf(buf, "%sAccept: %s\r\n", buf, accept_type);
+    sprintf(buf, "%sAccept-Encoding: %s\r\n", buf, accept_encoding);
+    sprintf(buf, "%sConnection: close\r\n", buf);
+    sprintf(buf, "%sProxy-Connection: close\r\n", buf);
 
     Rio_writen(net_fd, buf, strlen(buf));
 
@@ -226,6 +248,11 @@ void make_request(int fd, char *host, char *path)
     {
         read_return = Rio_readnb(&rio, reply, MAXBUF);
         //dbg_printf("READ_RETURN = %d\n", read_return);
+
+	//printf("\n==============================\n");
+	//printf("==============================\n");
+	//read_headers(&rio);
+	//Rio_writen(1, reply, read_return);
         Rio_writen(fd, reply, read_return);
     }while( read_return > 0);
 
